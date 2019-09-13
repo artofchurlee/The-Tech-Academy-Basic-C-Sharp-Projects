695a2b833393dfa90d6e515cf4ba207f9d78d20a
artofchurlee <lwu010@ucr.edu>
2019-09-13 15:33:01 -0400

    Upload do while & while loops

---
+++ While Loop/While Loop/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //WHILE LOOP
+
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            bool canBuy = age >= 21;
+            while (!canBuy)
+            {
+                Console.WriteLine("You cannot purchase alcohol.");
+                break;
+            }
+            if (canBuy)
+            {
+                Console.WriteLine("You can purchase alcohol.");
+            }
+
+            Console.ReadLine();
+
+            //DO WHILE LOOP
+            Console.WriteLine("How many more days until your birthday?");
+            int bday = Convert.ToInt32(Console.ReadLine());
+            bool isBday = bday == 0;
+
+            do
+            {
+                switch (bday)
+                {
+                    default:
+                        Console.WriteLine("Sorry, come back in " + bday + " days!");
+                        Console.ReadLine();
+                        break;
+                        
+
+                    case 1:
+                        Console.WriteLine("Wow, it's almost your birthday!");
+                        Console.ReadLine();
+                        break;
+
+                    case 0:
+                        Console.WriteLine("Happy Birthday!");
+
+                        break;
+
+                }
+
+            } while (!isBday);
+            
+
+            Console.Read();
+            
+        }   
+    }
+}
