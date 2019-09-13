0b52dacb5e1b7414717959c2a0163f860a5f3afb
artofchurlee <lwu010@ucr.edu>
2019-09-13 13:14:44 -0400

    Upload shipping quote program

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int roomTemp = 70;
+
+            Console.WriteLine("Hi, what is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hi " + name + ", what is the temperature where you are at?");
+            int currentTemp = Convert.ToInt32(Console.ReadLine());
+
+            //int currentTemp = 80;
+            //int roomTemp = 70;
+            //string comparisonResult = currentTemp == roomTemp ? "It is room temp." : "It is not room temp";
+        }
+    }
+}
---
+++ ShippingQuote/ShippingQuote/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Pacakge Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter package weight:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            if (weight > 50)
+            {
+                Console.WriteLine("Package is too heavy to be shipped via Package Express. Have a good day");
+            }
+            else {
+                Console.WriteLine("Please enter the package width:");
+                int width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package height:");
+                int height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package length:");
+                int length = Convert.ToInt32(Console.ReadLine());
+                float est = (width + height + length) * weight / 100.00f;
+                if (width + height + length > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via package express");
+                }
+                else
+                {
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + String.Format("{0:0.00}", est));
+                    Console.WriteLine("Thank you");
+                }
+
+                }
+            Console.ReadLine();
+
+        }
+    }
+}
