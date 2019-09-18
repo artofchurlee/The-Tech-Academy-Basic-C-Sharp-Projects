6dcabc082c535183d6103e71deb971c5e5020ed9
artofchurlee <lwu010@ucr.edu>
2019-09-18 15:35:48 -0400

    Upload Class practice #2

---
+++ ClassPractice2/ClassPractice2/ClassMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice2
+{
+    public class ClassMethod
+    {
+        public static void Para(int a, int b)
+        {
+
+            //     #1
+
+            int c = a * 2;
+            Console.WriteLine(b);
+            return;
+        }
+    }
+}
---
+++ ClassPractice2/ClassPractice2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //   #2-3
+            int a = 15;
+            int b = 25;
+            ClassMethod.Para(a, b);
+
+
+            //   #4
+            ClassMethod.Para(a: 15, b: 25);
+
+            Console.ReadLine();
+        }
+    }
+}
