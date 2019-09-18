2bb93596d21c066c35ff38312607c143144118d9
artofchurlee <lwu010@ucr.edu>
2019-09-18 13:27:45 -0400

    Upload practice with methods and classes

---
+++ ClassPractice/ClassPractice/IntClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice
+{
+    public class IntClass
+    {
+        public static int Addition(int v)
+        {
+            return (v + 15);
+
+        }
+        
+        public static int Subtraction(int v)
+        {
+            return (v - 2);
+        }
+
+        public static int Multiplication(int v)
+        {
+            return (v * 2);
+        }
+
+    }
+    
+}
---
+++ ClassPractice/ClassPractice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int v = Convert.ToInt32(Console.ReadLine());
+
+            int int2 = IntClass.Addition(v);
+
+            Console.WriteLine("Your number plus 15 is: " + int2);
+
+            int int3 = IntClass.Subtraction(v);
+            Console.WriteLine("Your number minus 2 is: " + int3);
+
+            int int4 = IntClass.Multiplication(v);
+            Console.WriteLine("Your number times 2 is: " + int4);
+
+
+            Console.ReadLine();
+        }
+    }
+}
