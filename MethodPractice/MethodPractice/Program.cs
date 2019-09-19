a2b2b7f32ef3f98be9cf336fa75f63fa1398846a
artofchurlee <lwu010@ucr.edu>
2019-09-19 16:45:56 -0400

    Upload overload method exercise

---
+++ MethodPractice/MethodPractice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int v = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another number.");
+            int w = Convert.ToInt32(Console.ReadLine());
+            VoidMethod.Div(v, w);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodPractice/MethodPractice/VoidMethod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice
+{
+    public static class VoidMethod
+    {
+        public static int Div(int v)
+        {
+            int c = v / 2;
+            Console.WriteLine(c);
+            return c;
+        }
+
+        public static int Div(int v, int w)
+        {
+            int c = v * w / 2;
+            Console.WriteLine(c);
+            return c;
+        }
+    }
+}
