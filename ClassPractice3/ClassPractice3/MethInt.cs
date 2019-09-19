56e4a07c3e23f14dff6b8c26cae38708f97543c4
artofchurlee <lwu010@ucr.edu>
2019-09-18 20:24:43 -0400

    Upload overloaded code

---
+++ ClassPractice3/ClassPractice3/MethInt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice3
+{
+    public class MethInt
+    {
+        public static int AddInt(int v)
+        {
+            int num1 = v + 12;
+            Console.WriteLine("Your number plus 12 is : " + num1);
+            return num1;
+        }
+        public static double AddInt(double v)
+        {
+            double num2 = v - 1.0;
+            Console.WriteLine("Your number minus 1 is : " + num2);
+            return num2;
+        }
+        public static string AddInt(string v)
+        {
+            double num3 = Convert.ToDouble(v);
+            double num4 = num3 * 5;
+            Console.WriteLine("Your number times 5 is : " + num4);
+
+            return Convert.ToString(num4);
+        }
+    }
+}
---
+++ ClassPractice3/ClassPractice3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(MethInt.AddInt(14));
+            Console.WriteLine(MethInt.AddInt(1.1));
+            Console.WriteLine(MethInt.AddInt("19.0"));
+
+            Console.ReadLine();
+        }
+    }
+}
