77ce0bd142d14983e29cd7bce30f3a5b6f02e2ae
artofchurlee <lwu010@ucr.edu>
2019-09-19 12:28:39 -0400

    Upload exercise with default

---
+++ TwoInt/TwoInt/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide a number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please provide another number. This is optional. Press enter to skip.");
+            if
+                (int.TryParse(Console.ReadLine(), out int b))
+                    {
+                TwoPara.Addition(a, b);
+            }
+            else
+            {
+
+                TwoPara.Addition(a);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TwoInt/TwoInt/TwoPara.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoInt
+{
+    public class TwoPara
+    {
+        public static int Addition(int a, int b = 1)
+        {
+            int c = a * b * 5;
+            Console.WriteLine("Your input times 5 is : " + c);
+            return c;
+        }
+    }
+}
