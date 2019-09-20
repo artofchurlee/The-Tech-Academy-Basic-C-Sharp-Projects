292c83f4ed4c50dde89ec63f6e00ecab0fb3d3c2
artofchurlee <lwu010@ucr.edu>
2019-09-20 12:23:01 -0400

    Upload Property practice

---
+++ PersonPractice/PersonPractice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Sample";
+            person.LastName = "Student";
+            person.SayName();
+
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Employee";
+            employee.SayName();
+
+            Console.ReadLine();
+
+        }
+    }
+}
