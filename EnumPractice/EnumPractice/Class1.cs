74509382d44bef3c9ebec859f276279a6a382c97
artofchurlee <lwu010@ucr.edu>
2019-09-23 13:21:02 -0400

    upload enum practice

---
+++ EnumPractice/EnumPractice/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumPractice
+{
+    public enum DayofWeek
+    {
+        Sunday = 7,
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday= 4,
+        Friday = 5,
+        Saturday = 6
+    }
+}
---
+++ EnumPractice/EnumPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the day of the week it is.");
+            string input = Console.ReadLine();
+            try
+            {
+                Enum.Parse(typeof(DayofWeek), input, true);
+                Console.WriteLine("Today is " + input);
+            }
+
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
