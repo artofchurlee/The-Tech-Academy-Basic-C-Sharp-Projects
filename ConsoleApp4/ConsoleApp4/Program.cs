fa02da26f330d93aa2066c515b1303d24d26fe29
artofchurlee <lwu010@ucr.edu>
2019-09-23 11:59:35 -0400

    Upload generic practice

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            employee.Things = new List<string>() { "George", "John", "Jason", "John", "Thomas" };
+
+            Employee<int> ID = new Employee<int>();
+            ID.Things = new List<int>() {  1, 2, 3, 5, 8 };
+
+            for (int i = 0; i < ID.Things.Count; i++)
+            {
+                Console.WriteLine(employee.Things[i]);
+                Console.WriteLine(ID.Things[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
