119441857c6a2102dc46923a19a2c72232f6fecd
artofchurlee <lwu010@ucr.edu>
2019-09-25 21:17:43 -0400

    Upload datetime practice

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime timeNow = DateTime.Now;
+            Console.WriteLine("Right now it is " + timeNow);
+            
+            Console.WriteLine("Please provide a number to be added to the hour.");
+            int addhr = Convert.ToInt32(Console.ReadLine());
+            TimeSpan totalTime = new TimeSpan(addhr, 0, 0);
+            DateTime newTime = timeNow.Add(totalTime);
+            Console.WriteLine("The time in " + addhr + "hours will be " + newTime);
+
+            Console.ReadLine();
+
+        }
+    }
+}
