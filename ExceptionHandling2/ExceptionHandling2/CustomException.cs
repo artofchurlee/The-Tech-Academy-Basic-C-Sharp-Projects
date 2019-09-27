7788be1d2948700836570e21a1e4c775c805a11c
artofchurlee <lwu010@ucr.edu>
2019-09-27 12:57:14 -0400

    upload exception handling exercise

---
+++ ExceptionHandling2/ExceptionHandling2/CustomException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling2
+{
+    public class CustomException: Exception
+    {
+        public CustomException()
+            : base() { }
+        public CustomException(string message)
+            : base(message) { }
+    }
+    public class CustomException2: Exception
+    {
+        public CustomException2()
+            : base() { }
+        public CustomException2(string message)
+            : base(message) { }
+    }
+}
---
+++ ExceptionHandling2/ExceptionHandling2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please provide your age in years");
+                int yearsOld = Convert.ToInt32(Console.ReadLine());
+                if (yearsOld == 0) throw new CustomException();
+                if (yearsOld < 0) throw new CustomException2();
+                int yearBorn = 2019 - yearsOld;
+                Console.WriteLine("You were born in " + yearBorn);
+                Console.ReadLine();
+               
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Please enter a valid whole number.");
+                Console.ReadLine();
+                return;
+            }
+            catch(CustomException)
+            {
+                Console.WriteLine("We all know babies can't run this program.");
+                Console.ReadLine();
+                return;
+            }
+            catch(CustomException2)
+            {
+                Console.WriteLine("Hello time traveler. Our code only works for people born in the past");
+                Console.ReadLine();
+                return;
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("An Error has occured. Please try again later.");
+                Console.ReadLine();
+                return;
+            }
+
+
+
+        }
+    }
+}
