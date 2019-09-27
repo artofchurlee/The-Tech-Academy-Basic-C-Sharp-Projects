e9fd7d6302409c032c998af307554de979c72c23
artofchurlee <lwu010@ucr.edu>
2019-09-27 11:50:13 -0400

    upload const practice

---
+++ ConstVariables/ConstVariables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "Shirley Wu";
+
+
+            var mySib = new List<string>() { "Katrina", "Andy", "Anna" };
+
+
+        }
+
+        public class Name
+        {
+            public Name(string name, int age)
+            {
+                Console.WriteLine("Your name is " + name + "and you are " + age + " years old.");
+            }
+            //I guess if they're too afraid to tell us their age we just fill it in with 0
+            public Name(string name) : this(name, 0)
+            { }
+        }
+
+    }
+}
